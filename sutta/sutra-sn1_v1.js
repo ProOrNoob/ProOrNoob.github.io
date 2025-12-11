71be2d75a9a11c50c9f57cdeaf5c04d23515e53b
LonelyLife <117442709+ProOrNoob@users.noreply.github.com>
2025-12-11 11:13:29 +0700

    Update text entries in sutra-sn1_v1.js

--- sutta/sutra-sn1_v1.js
+++ sutta/sutra-sn1_v1.js
@@ -14,9 +14,9 @@ window.SUTRA_DATA['sn1_v1'] = {
       vie: 'Như vầy tôi nghe. Một thời, Thế Tôn đang đi trên con đường giữa Ràjagaha (Vương Xá) và Nalandà, cùng với đại chúng Tỷ-kheo khoảng năm trăm vị. Lúc bấy giờ có Suppiya du sĩ ngoại đạo cũng đi trên con đường giữa Ràjagaha và Nalandà cùng với đệ tử, thanh niên Brahmadatta. Suppiya, du sĩ ngoại đạo dùng vô số phương tiện hủy báng Phật, hủy báng Pháp, hủy báng Tăng, còn đệ tử của du sĩ ngoại đạo Suppiya, thanh niên Brahmadatta, lại dùng vô số phương tiện tán thán Phật, tán thán Pháp, tán thán Tăng. Như vậy cả hai thầy trò nói những lời hoàn toàn tương phản nhau trong khi đi sau lưng đức Phật và chúng Tỷ-kheo.'
     },
     {
-      pali: 'Tena kho pana samayena āyasmā Ānando...',
-      eng: 'At that time, Venerable Ānanda...',
-      vie: 'Lúc bấy giờ, Tôn giả A Nan...'
+      pali: '',
+      eng: '',
+      vie: '2. Rồi Thế Tôn đến nghỉ một đêm tại Ambalatthikà, nhà nghỉ mát của vua, cùng với chúng Tỷ-kheo. Suppiya, du sĩ ngoại đạo cũng đến nghỉ một đêm tại Ambalatthikà, nhà nghỉ mát của vua, cùng với đệ tử, thanh niên Brahmadatta. Tại đây, du sĩ ngoại đạo Suppiya cũng dùng vô số phương tiện hủy báng Phật, hủy báng Pháp, hủy báng Tăng, còn người đệ tử của du sĩ ngoại đạo Suppiya, thanh niên Brahmadatta, lại dùng vô số phương tiện tán thán Phật, tán thán Pháp, tán thán Tăng. Như vậy cả hai thầy trò nói những lời tương phản nhau trong khi đi sau lưng Thế Tôn và chúng Tỷ-kheo.'
     }
   ]
 };
