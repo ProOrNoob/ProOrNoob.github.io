5d3f06161343327f8eecbba93fcf6bca958af6d9
LonelyLife <117442709+ProOrNoob@users.noreply.github.com>
2025-12-11 10:50:19 +0700

    Update Vietnamese titles in sutra-sn1_v1.js

---
+++ sutta/sutra-sn1_v1.js
@@ -0,0 +1,22 @@
+window.SUTRA_DATA = window.SUTRA_DATA || {};
+
+window.SUTRA_DATA['sn1_v1'] = {
+  titleVi: 'TEST',
+  titleEn: 'MN 132 – Ānanda Sutta',
+
+  subtitleVi: 'Kinh TEST',
+  subtitleEn: 'Discourse to Ānanda',
+
+  rows: [
+    {
+      pali: 'Sāvatthiyaṁ viharati bhagavā...',
+      eng: 'The Blessed One was staying near Sāvatthī...',
+      vie: 'Một thời, Thế Tôn trú tại Xá-vệ...'
+    },
+    {
+      pali: 'Tena kho pana samayena āyasmā Ānando...',
+      eng: 'At that time, Venerable Ānanda...',
+      vie: 'Lúc bấy giờ, Tôn giả A Nan...'
+    }
+  ]
+};
