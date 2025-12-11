bb1f0925eb1d156733d37de1ff1fb23a7681ed2a
LonelyLife <117442709+ProOrNoob@users.noreply.github.com>
2025-12-11 11:20:12 +0700

    Update sutra-sn1_v1.js

--- sutta/sutra-sn1_v1.js
+++ sutta/sutra-sn1_v1.js
@@ -1,16 +1,26 @@
 window.SUTRA_DATA = window.SUTRA_DATA || {};
 
 window.SUTRA_DATA['sn1_v1'] = {
-  titleVi: 'Kinh Phạm võng',
-  titleEn: 'MN 132 – Ānanda Sutta',
+  titleVi: 'Brahmajālasutta',
+  titleEn: 'Brahmajālasutta',
 
   subtitleVi: 'Kinh Phạm võng',
-  subtitleEn: 'Discourse to Ānanda',
+  subtitleEn: 'The Divine Net',
 
   rows: [
     {
-      pali: '',
-      eng: '',
+      pali: 'Evaṁ me sutaṁ—
+ekaṁ samayaṁ bhagavā antarā ca rājagahaṁ antarā ca nāḷandaṁ addhānamaggappaṭipanno hoti mahatā bhikkhusaṅghena saddhiṁ pañcamattehi bhikkhusatehi.
+Suppiyopi kho paribbājako antarā ca rājagahaṁ antarā ca nāḷandaṁ addhānamaggappaṭipanno hoti saddhiṁ antevāsinā brahmadattena māṇavena.
+Tatra sudaṁ suppiyo paribbājako anekapariyāyena buddhassa avaṇṇaṁ bhāsati, dhammassa avaṇṇaṁ bhāsati, saṅghassa avaṇṇaṁ bhāsati;
+suppiyassa pana paribbājakassa antevāsī brahmadatto māṇavo anekapariyāyena buddhassa vaṇṇaṁ bhāsati, dhammassa vaṇṇaṁ bhāsati, saṅghassa vaṇṇaṁ bhāsati.
+Itiha te ubho ācariyantevāsī aññamaññassa ujuvipaccanīkavādā bhagavantaṁ piṭṭhito piṭṭhito anubandhā honti bhikkhusaṅghañca.',
+      eng: 'So I have heard.
+At one time the Buddha was traveling along the road between Rājagaha and Nāḷandā together with a large Saṅgha of five hundred mendicants.
+The wanderer Suppiya was also traveling along the same road, together with his resident pupil, the student Brahmadatta.
+Meanwhile, Suppiya criticized the Buddha, the teaching, and the Saṅgha in many ways,
+but his pupil Brahmadatta praised them in many ways.
+And so both tutor and pupil followed behind the Buddha and the Saṅgha of mendicants directly contradicting each other.',
       vie: 'Như vầy tôi nghe. Một thời, Thế Tôn đang đi trên con đường giữa Ràjagaha (Vương Xá) và Nalandà, cùng với đại chúng Tỷ-kheo khoảng năm trăm vị. Lúc bấy giờ có Suppiya du sĩ ngoại đạo cũng đi trên con đường giữa Ràjagaha và Nalandà cùng với đệ tử, thanh niên Brahmadatta. Suppiya, du sĩ ngoại đạo dùng vô số phương tiện hủy báng Phật, hủy báng Pháp, hủy báng Tăng, còn đệ tử của du sĩ ngoại đạo Suppiya, thanh niên Brahmadatta, lại dùng vô số phương tiện tán thán Phật, tán thán Pháp, tán thán Tăng. Như vậy cả hai thầy trò nói những lời hoàn toàn tương phản nhau trong khi đi sau lưng đức Phật và chúng Tỷ-kheo.'
     },
     {
