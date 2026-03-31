79e0f1c7de951c4e72750b49600732f9dec7b090
proornoob <luisdiesel@live.com>
2026-03-31 11:42:57 +0700

    add md

--- sutta/pli/mn01 - Copy (7).js
+++
@@ -1,5 +0,0 @@
-window.BILARA = window.BILARA || {};
-window.BILARA["mn01"] = window.BILARA["mn01"] || {};
-window.BILARA["mn01"]["pli"] =
-window.BILARA["mn01"]["pli"]["mn01:source"] =
-  "SOURCE: https://suttacentral.net/ ";
