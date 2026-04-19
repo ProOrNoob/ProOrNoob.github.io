6f054a92f817af0e6e11fc1cf1ca77c20ad734e7
proornoob <luisdiesel@live.com>
2026-04-20 03:31:12 +0700

    add suttra

---
+++ sutta/vi/sn17_v1.js
@@ -0,0 +1,5 @@
+window.BILARA = window.BILARA || {};
+window.BILARA["sn17_v1"] = window.BILARA["sn17_v1"] || {};
+window.BILARA["sn17_v1"]["vi"] =
+window.BILARA["sn17_v1"]["vi"]["sn17_v1:source"] =
+ "Dịch từ nguồn Pali https://suttacentral.net/ .Trong quá trình dịch có thể có sai sót, vui lòng đối chiếu bản gốc Pali, và tiếng Anh. Mọi góp ý chỉnh sửa xin vui lòng email: tuanctvn199@gmail.com ";
---
+++ sutta/vi/sn43_v1.js
@@ -0,0 +1,5 @@
+window.BILARA = window.BILARA || {};
+window.BILARA["sn43_v1"] = window.BILARA["sn43_v1"] || {};
+window.BILARA["sn43_v1"]["vi"] =
+window.BILARA["sn43_v1"]["vi"]["sn43_v1:source"] =
+ "Dịch từ nguồn Pali https://suttacentral.net/ .Trong quá trình dịch có thể có sai sót, vui lòng đối chiếu bản gốc Pali, và tiếng Anh. Mọi góp ý chỉnh sửa xin vui lòng email: tuanctvn199@gmail.com ";
