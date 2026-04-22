66fbdaa1dac8f3d65bbb9209e089e81e29edc81d
proornoob <luisdiesel@live.com>
2026-04-22 23:44:58 +0700

    full sutta

--- sutta/vi/dn21.js
+++ sutta/vi/dn21.js
@@ -519,4 +519,4 @@ window.BILARA["dn21"]["vi"] = {
 }
 
 window.BILARA["dn21"]["vi"]["dn21:source"] =
-  "Dịch từ nguồn Pali https://suttacentral.net/";
+ "Dịch từ nguồn Pali https://suttacentral.net/ .Trong quá trình dịch có thể có sai sót, vui lòng đối chiếu bản gốc Pali, và tiếng Anh. Mọi góp ý chỉnh sửa xin vui lòng email: tuanctvn199@gmail.com ";
